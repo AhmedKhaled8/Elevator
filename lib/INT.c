3743733cb2424b4037cbd78ae3508578022e7491
Ahmad <ahmedmageed19@gmail.com>
2020-04-13 23:57:35 +0200

    edit in interrupt init

--- lib/INT.c
+++ lib/INT.c
@@ -1,19 +1,19 @@
 #include <INT.h>
 
 
+
 void int_INIT(void) {
     // setting interrupt zero 
     IT0 = 1 ; // type Edge Sensitive 
-    PX0 = 1 ; // Set HIGH priority  
+    // PX0 = 1 ; // Set HIGH priority  
     // setting interrupt one
     IT1 = 1; // type Edge Sensitive
-    PX1 = 1 ; // Set HIGH priority  
+    // PX1 = 1 ; // Set HIGH priority  
     
     // Enable 
-    EX0 = 1 ; 
-    EX1 = 1 ; 
-
-    // Global Interrupt Flag
-    EA = 1; 
+    // EX0 = 1; 
+    // EA = 1 ;
+    // EX1 = 1 ;
+    IE = 0x85; 
 }
 
